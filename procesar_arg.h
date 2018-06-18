1b177991370231c0df4432470fe04a4b3f3b14ab
RosarioSzuplat <37803380+RosarioSzuplat@users.noreply.github.com>
2018-06-18 10:09:12 -0300

    falta que me responda moreno

---
+++ procesar_arg.h
@@ -0,0 +1,69 @@
+#ifndef PROCESAR_ARG__H
+#define PROCESAR_ARG__H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MIN_ARGC 2
+#define MEMORIA_POR_OMISION 50
+#define TXT_ARG_MEMORIA "-m"
+#define TXT_ARG_FMT_SALIDA "-f"
+#define TXT_ARG_AYUDA "-h"
+#define TXT_INDICADOR_BINARIO "bin"
+#define TXT_INDICADOR_TEXTO "txt"
+#define MAX_MODIFICADORES 2
+#define POS_FMT 0
+#define INDICADOR_STDIN '-'
+#define INDICADOR_BIN 'b'
+#define INDICADOR_TXT 't'
+#define ARCHIVO_POR_OMISION "stdin"
+#define DELIM ':'
+#define MAX_STR 50
+#define SIZE_ARCHIVO_OMISION 5
+
+typedef enum {
+	ARG_MEMORIA = 0,
+	ARG_FMT_SALIDA = 1,
+	ARG_AYUDA = 2,
+	ARG_INVALIDO = 3
+}arg_t;
+
+typedef enum {
+	ST_OK,
+	ST_ERROR_PUNTERO_NULO,
+	ST_ERROR_CANTIDAD_ARGUMENTOS,
+	ST_ERROR_MEMORIA_INVALIDA,
+	ST_ERROR_ENTRADA_INVALIDA,
+	ST_ERROR_FILE_INPUT_DEMASIADO_LARGO,
+	ST_ERROR_FILE_OUTPUT_DEMASIADO_LARGO,
+	ST_ERROR_ARGUMENTO_INVALIDO,
+	ST_ERROR_INSTRUCCION_INVALIDA,
+	ST_ERROR_LECTURA_ARCHIVO,
+	ST_ERROR_CANTIDAD_DE_MEMORIA_INVALIDA,
+	ST_ERROR_MEMORIA_INSUFICIENTE_ARCHIVO,
+	ST_ERROR_SIMPLETRON,
+	ST_HELP
+} status_t;
+
+typedef enum{
+	FMT_TXT = 0,
+	FMT_BIN = 1
+} fmt_t;
+
+typedef struct{
+	fmt_t fmt_entrada;
+	char *nombre_archivo;
+}archivos;
+
+typedef struct{
+	size_t cant_memoria;
+	fmt_t fmt_salida;
+	int cant_archivos;
+	archivos *vector_datos_archivos;
+}params;
+
+status_t procesar_argumentos(const int argc, const char *argv [], params *parametros, int *cant_archivos);
+status_t cargar_argumentos_por_omision (params *parametros);
+
+#endif
