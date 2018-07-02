2823d52c76b4e8134646a962162498fdb48657c3
Santiago Lopez <santiago.lopez.497@gmail.com>
2018-07-02 16:02:59 -0300

    Comenté las primitivas de la lista tda que mandó Moreno
    Tanto el .c de recursividad como el iterativo

--- tda_lista_ite.c
+++ tda_lista_ite.c
@@ -15,11 +15,13 @@
 #include <stdlib.h>
 #include "../lib/types.h"
 #include "tda_lista.h"
-
+/* Recibe una lista. En caso de que esté vacía devuelve TRUE,
+ * si no lo está devuelve FALSE */
 bool_t LISTA_esta_vacia(lista_t lista) {
     return lista == NULL;
 }
-
+/* Recibe un puntero a una lista. Deuvelve por la interfaz una lista
+ * vacía y por el nombre un estado */
 retval_t LISTA_crear(lista_t * plista) {
     if(plista == NULL)
         return RV_ILLEGAL;
@@ -28,7 +30,10 @@ retval_t LISTA_crear(lista_t * plista) {
 
     return RV_SUCCESS;
 }
-
+/* Recibe un puntero doble a un nodo y un dato. Crea un nodo, guardando
+ * el dato recibido y un puntero al siguiente nodo, inicializándolo en NULL,
+ * indicando el final de la lista. Devuelve El nodo por la interfaz y un
+ * estado por el nombre */
 retval_t LISTA_crear_nodo(nodo_t ** pnodo, void * dato) {
     if(pnodo == NULL)
         return RV_ILLEGAL;
@@ -41,7 +46,13 @@ retval_t LISTA_crear_nodo(nodo_t ** pnodo, void * dato) {
 
     return RV_SUCCESS;
 }
-
+/* Recibe un puntero doble a un nodo y un puntero a una función que sabrá
+ * destruir el dato almacenado. Guarda la dirección del dato almacenado 
+ * en el nodo en un puntero a void y apunta a los punteros del nodo a
+ * NULL, y luego libera el puntero al nodo. Finalmente, si el puntero a 
+ * la función de destrucción no es nulo llama a la función y devuelve su
+ * estado por el nombre; de ser nulo el puntero devuelve RV_SUCCESS por 
+ * el nombre */
 retval_t LISTA_destruir_nodo(nodo_t ** pnodo, retval_t (*destructor_dato)(void *)) {
     void * dato;
 
@@ -60,7 +71,15 @@ retval_t LISTA_destruir_nodo(nodo_t ** pnodo, retval_t (*destructor_dato)(void *
 
     return (destructor_dato != NULL) ? (*destructor_dato)(dato) : RV_SUCCESS;
 }
-
+/* Recibe un puntero a una lista y un puntero a una función que destruye
+ * el dato al que apunta el nodo. Si el puntero al primer elemento apunta
+ * a NULL se informa que la función finalizó con éxito por el nombre y se
+ * devuelve la nueva lista por la interfaz.
+ * Guarda la dirección a la que apunta el puntero al inicio de la lista
+ * en un puntero a nodo_t y el puntero al principio de la lista se lo 
+ * apunta al siguiente elemento de la lista. 
+ * Al final se llama a la función LISTA_destruir_nodo y se destruye al 
+ * nodo con esa función */
 retval_t LISTA_destruir_primero(lista_t * plista, retval_t (*destructor_dato)(void *)) {
     nodo_t * primero;
 
@@ -75,7 +94,11 @@ retval_t LISTA_destruir_primero(lista_t * plista, retval_t (*destructor_dato)(vo
 
     return LISTA_destruir_nodo(&primero, destructor_dato);
 }
-
+/* Recibe un puntero a la lista y un puntero a una función que destruye el
+ * dato al que apunta el nodo. Mientras la lista no esté vacía se guarda
+ * el puntero al siguiente elemento de la lista y se destruye el nodo
+ * actual con LISTA_destruir_nodo 
+ * Devuelve un estado por el nombre y la lista actualizada por la interfaz */
 retval_t LISTA_destruir(lista_t * plista, retval_t (*destructor_dato)(void *)) {
     nodo_t * nodo_a_borrar;
     retval_t rv;
@@ -93,7 +116,11 @@ retval_t LISTA_destruir(lista_t * plista, retval_t (*destructor_dato)(void *)) {
 
     return RV_SUCCESS;
 }
-
+/* Recibe un puntero a una lista y un puntero a un dato a insertar al
+ * comienzo de la lista. Se crea un nodo en el cual se guarda el puntero
+ * al dato y se lo apunta al primer elemento de la lista, también apuntando
+ * el puntero al inicio de la lista al nuevo nodo.
+ * Se devuelve la nueva lista por la interfaz y un estado por el nombre */
 retval_t LISTA_insertar_al_ppio(lista_t * plista, void * dato) {
     nodo_t * nuevo;
     retval_t rv;
@@ -109,7 +136,14 @@ retval_t LISTA_insertar_al_ppio(lista_t * plista, void * dato) {
 
     return RV_SUCCESS;
 }
-
+/* Recibe un puntero a una lista y un puntero a un dato a insertar al final
+ * de la lista. Se crea un nodo en el cual se guarda el puntero al dato.
+ * Si la lista está vacía se apunta el puntero a la lista al nodo creado.
+ * Si no lo está se itera avanzando por la lista hasta que el puntero al
+ * siguiente nodo sea nulo, utilizando un puntero a nodo auxiliar, y se
+ * guarda el nodo nuevo en el puntero al siguiente del último nodo de la
+ * lista. Devuelve un estado por el nombre y la lista actualizada por la
+ * interfaz */
 retval_t LISTA_insertar_al_final(lista_t * plista, void * dato) {
     retval_t rv;
     nodo_t * nuevo, *temp;
@@ -130,7 +164,15 @@ retval_t LISTA_insertar_al_final(lista_t * plista, void * dato) {
 
     return RV_SUCCESS;
 }
-
+/* Recibe un puntero a una lista previamente ordenada, un puntero a un
+ * dato y una función que compara datos. se crea un nodo y se guarda el
+ * dato en ese nodo. Si la lista está vacía o si la función de comparación
+ * es menor a 0, se inserta el nodo al comienzo de la lista. De otra forma,
+ * se itera guardando los punteros a los nodos actual y anterior hasta que
+ * la función de comparación indique que el elemento a guardar es mayor al
+ * siguiente elemento de la lista. Se termina por guardar el nodo creado
+ * en el lugar del nodo actual, y elnodo actual a continuación del creado.
+ * Devuelve la lista actualizada por la interfaz y un estado por el nombre */
 retval_t LISTA_insertar_decreciente(lista_t * plista, void * dato, int (*cmp)(void *, void *)) {
     retval_t rv;
     nodo_t * nuevo, * actual, *anterior;
@@ -161,7 +203,12 @@ retval_t LISTA_insertar_decreciente(lista_t * plista, void * dato, int (*cmp)(vo
 
     return RV_SUCCESS;
 }
-
+/* Recibe un puntero a un nodo, un dato a buscar y un puntero a una función
+ * que indica si dos elementos son iguales. Itera llamando a la función,
+ * y si esta es distinta de 0, devuelve un puntero al dato encontrado. 
+ * De no ser iguales apunta el puntero al siguiente nodo al puntero al
+ * nodo actual.
+ * De no encontrar el elemento, devuelve NULL */
 void * LISTA_buscar(lista_t plista, void * t, int (*es_igual)(void *, void *)) {
     if(plista == NULL || es_igual == NULL)
         return NULL;
@@ -174,7 +221,12 @@ void * LISTA_buscar(lista_t plista, void * t, int (*es_igual)(void *, void *)) {
 
     return NULL;
 }
-
+/* Recibe un puntero a un nodo, el stream en donde imprimir y un puntero
+ * a una función que imprime el tipo de dato guardado en la lista. Itera
+ *  mientras imprime el dato llamando a la función pasada como argumento
+ * y cambiando guardando el puntero al siguiente nodo en el puntero al nodo
+ * actual.
+ * Devuelve un estado por el nombre */
 retval_t LISTA_imprimir(lista_t plista, FILE * ofile, retval_t (*impresor)(void *, FILE *)) {
     if(impresor != NULL) {
         while(plista != NULL) {
@@ -187,6 +239,12 @@ retval_t LISTA_imprimir(lista_t plista, FILE * ofile, retval_t (*impresor)(void 
 }
 
 /* con esta implementación de XX_recorrer, se puede imprimir también */
+/* Recibe un puntero a un nodo, un puntero a una función la cual llevará
+ * a cabo la operación deseada por el usuario y un puntero a un argumento
+ * para dicha función. Se itera mientras no sea nulo el puntero al nodo,
+ * llamando a la función pasada como argumento con el dato del nodo y el
+ * argumento pasado como argumento. 
+ * Devuelve un estado por el nombre */
 retval_t LISTA_recorrer(lista_t plista, retval_t (*funcion)(void *, void *), void * argumento) {
     if(funcion != NULL) {
         while(plista != NULL) {
@@ -200,6 +258,12 @@ retval_t LISTA_recorrer(lista_t plista, retval_t (*funcion)(void *, void *), voi
 
 /* crea una nueva lista con los nodos de la lista original que al aplicarles el
  * filtro dan true */
+/* Recibe una lista, un puntero a una lista nueva y una función la cual
+ * indica si el dato actual cumple o no con determinada característica.
+ * Crea una lista nueva e inserta al final de ella los elementos que hagan
+ * que la función pasada como argumento devuelva true.
+ * En caso de fallar destruye la lista creada.
+ * Devuelve un estado por el nombre y la lista nueva por la interfaz */
 retval_t LISTA_filtrar(lista_t plista, lista_t * pnueva, bool_t (*filtro)(void *)) {
     lista_t nueva;
     lista_t * pfinal;
--- tda_lista_rec.c
+++ tda_lista_rec.c
@@ -16,10 +16,13 @@
 #include "../lib/types.h"
 #include "tda_lista.h"
 
+/* Recibe una lista. En caso de que esté vacía devuelve TRUE,
+ * si no lo está devuelve FALSE */
 bool_t LISTA_esta_vacia(lista_t lista) {
     return lista == NULL;
 }
-
+/* Recibe un puntero a una lista. Deuvelve por la interfaz una lista
+ * vacía y por el nombre un estado */
 retval_t LISTA_crear(lista_t * plista) {
     if(plista == NULL)
         return RV_ILLEGAL;
@@ -28,7 +31,10 @@ retval_t LISTA_crear(lista_t * plista) {
 
     return RV_SUCCESS;
 }
-
+/* Recibe un puntero doble a un nodo y un dato. Crea un nodo, guardando
+ * el dato recibido y un puntero al siguiente nodo, inicializándolo en NULL,
+ * indicando el final de la lista. Devuelve El nodo por la interfaz y un
+ * estado por el nombre */
 retval_t LISTA_crear_nodo(nodo_t ** pnodo, void * dato) {
     if(pnodo == NULL)
         return RV_ILLEGAL;
@@ -41,7 +47,13 @@ retval_t LISTA_crear_nodo(nodo_t ** pnodo, void * dato) {
 
     return RV_SUCCESS;
 }
-
+/* Recibe un puntero doble a un nodo y un puntero a una función que sabrá
+ * destruir el dato almacenado. Guarda la dirección del dato almacenado 
+ * en el nodo en un puntero a void y apunta a los punteros del nodo a
+ * NULL, y luego libera el puntero al nodo. Finalmente, si el puntero a 
+ * la función de destrucción no es nulo llama a la función y devuelve su
+ * estado por el nombre; de ser nulo el puntero devuelve RV_SUCCESS por 
+ * el nombre */
 retval_t LISTA_destruir_nodo(nodo_t ** pnodo, retval_t (*destructor_dato)(void *)) {
     void * dato;
 
@@ -60,7 +72,15 @@ retval_t LISTA_destruir_nodo(nodo_t ** pnodo, retval_t (*destructor_dato)(void *
 
     return (destructor_dato != NULL) ? (*destructor_dato)(dato) : RV_SUCCESS;
 }
-
+/* Recibe un puntero a una lista y un puntero a una función que destruye
+ * el dato al que apunta el nodo. Si el puntero al primer elemento apunta
+ * a NULL se informa que la función finalizó con éxito por el nombre y se
+ * devuelve la nueva lista por la interfaz.
+ * Guarda la dirección a la que apunta el puntero al inicio de la lista
+ * en un puntero a nodo_t y el puntero al principio de la lista se lo 
+ * apunta al siguiente elemento de la lista. 
+ * Al final se llama a la función LISTA_destruir_nodo y se destruye al 
+ * nodo con esa función */
 retval_t LISTA_destruir_primero(lista_t * plista, retval_t (*destructor_dato)(void *)) {
     nodo_t * primero;
 
@@ -75,7 +95,12 @@ retval_t LISTA_destruir_primero(lista_t * plista, retval_t (*destructor_dato)(vo
 
     return LISTA_destruir_nodo(&primero, destructor_dato);
 }
-
+/* Recibe un puntero a la lista y un puntero a una función que destruye el
+ * dato al que apunta el nodo. Si la lista está vacía devuelve un estado
+ * por el nombre y la lista vacía por la interfaz. De no estar vacía se
+ * guarda el puntero al siguiente elemento de la lista, se destruye el nodo
+ * actual con LISTA_destruir_nodo y se vuelve a llamar a la función con el
+ * siguiente nodo de la lista */
 retval_t LISTA_destruir(lista_t * plista, retval_t (*destructor_dato)(void *)) {
     nodo_t * siguiente;
 
@@ -90,7 +115,11 @@ retval_t LISTA_destruir(lista_t * plista, retval_t (*destructor_dato)(void *)) {
     LISTA_destruir_nodo(plista, destructor_dato );
     return LISTA_destruir(&siguiente, destructor_dato);
 }
-
+/* Recibe un puntero a una lista y un puntero a un dato a insertar al
+ * comienzo de la lista. Se crea un nodo en el cual se guarda el puntero
+ * al dato y se lo apunta al primer elemento de la lista, también apuntando
+ * el puntero al inicio de la lista al nuevo nodo.
+ * Se devuelve la nueva lista por la interfaz y un estado por el nombre */
 retval_t LISTA_insertar_al_ppio(lista_t * plista, void * dato) {
     nodo_t * nuevo;
     retval_t rv;
@@ -106,7 +135,11 @@ retval_t LISTA_insertar_al_ppio(lista_t * plista, void * dato) {
 
     return RV_SUCCESS;
 }
-
+/* Recibe un puntero a una lista y un puntero a un dato a insertar al final
+ * de la lista. Si la lista está vacía se llama a la función que ingresa
+ * al comienzo. Si no lo está, se vuelve a llamar a está función con el
+ * puntero al siguiente elemento.
+ * Devuelve un estado por el nombre y la lista actualizada por la interfaz */
 retval_t LISTA_insertar_al_final(lista_t * plista, void * dato) {
     if(plista == NULL)
         return RV_ILLEGAL;
@@ -116,7 +149,14 @@ retval_t LISTA_insertar_al_final(lista_t * plista, void * dato) {
 
     return LISTA_insertar_al_final(&((*plista)->siguiente), dato);
 }
-
+/* Recibe un puntero a una lista previamente ordenada, un puntero a un
+ * dato y una función que compara datos. Si la lista está vacía o si la
+ * función de comparación es menor a 0 se crea un nodo, se guarda el
+ * dato en ese nodo, y se inserta el nodo en el lugar previo al nodo en el
+ * que se encontraba el puntero de la lista.
+ * De otra forma, se vuelve a llamar a la función con el
+ * siguiente elemento de la lista y se vuelve a preguntar. 
+ * Devuelve la lista actualizada por la interfaz y un estado por el nombre */
 retval_t LISTA_insertar_decreciente(lista_t * plista, void * dato, int (*cmp)(void *, void *)) {
     retval_t rv;
     nodo_t * nuevo;
@@ -136,7 +176,10 @@ retval_t LISTA_insertar_decreciente(lista_t * plista, void * dato, int (*cmp)(vo
 
     return LISTA_insertar_decreciente(&((*plista)->siguiente), dato, cmp);
 }
-
+/* Recibe un puntero a un nodo, un dato a buscar y un puntero a una función
+ * que indica si dos elementos son iguales. Llama a la función y si esta
+ * es distinta de 0 devuelve un puntero al dato. De no ser iguales se vuelve
+ * a llamar a LISTA_buscar con el puntero al siguiente nodo */
 void * LISTA_buscar(lista_t pnodo, void * t, int (*es_igual)(void *, void *)) {
     if(pnodo == NULL)
         return NULL;
@@ -146,7 +189,11 @@ void * LISTA_buscar(lista_t pnodo, void * t, int (*es_igual)(void *, void *)) {
 
     return LISTA_buscar(pnodo->siguiente, t, es_igual);
 }
-
+/* Recibe un puntero a un nodo, el stream en donde imprimir y un puntero
+ * a una función que imprime el tipo de dato guardado en la lista. Imprime
+ * el dato llamando a la función pasada como argumento y luego vuelve a
+ * llamar a LISTA_imprimir hasta que el puntero al nodo sea nulo. 
+ * Devuelve un estado por el nombre */
 retval_t LISTA_imprimir(lista_t pnodo, FILE * ofile, retval_t (*impresor)(void *, FILE *)) {
     if(pnodo == NULL)
         return RV_SUCCESS;
@@ -156,7 +203,13 @@ retval_t LISTA_imprimir(lista_t pnodo, FILE * ofile, retval_t (*impresor)(void *
 
     return RV_SUCCESS;
 }
-
+/* Recibe un puntero a un nodo, un puntero a una función la cual llevará
+ * a cabo la operación deseada por el usuario y un puntero a un argumento
+ * para dicha función. De no ser nulo el puntero al nodo, se llama a la
+ * función pasada como argumento con el dato del nodo y el argumento pasado
+ * como argumento. Se vuelve a llamar a LISTA_recorrer con el siguiente
+ * puntero a nodo hasta que este sea nulo
+ * Devuelve un estado por el nombre */
 retval_t LISTA_recorrer(lista_t pnodo, retval_t (*funcion)(void *, void *), void * argumento) {
     if(pnodo == NULL)
         return RV_SUCCESS;
